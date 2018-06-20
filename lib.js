5130806950b6d0bc1fb7901585a7f3699797e8a3
prosmacs <35267599+prosmacs@users.noreply.github.com>
2018-06-20 17:02:49 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -1512,7 +1512,7 @@ initSteem():
 function initLib(initSteem, callback) {
   setupLogging();
   // #93, use alternate websocket temporarily
-  steem.api.setOptions({ url: 'steemd.minnowsupportproject.org'});
+  steem.api.setOptions({ url: 'https://rpc.steemliberator.com'});
   // #71, no longer need to set this
   var processes = [
     function() {
