e6bef59e170af7d0784df9ca6233f052bfaf0386
prosmacs <35267599+prosmacs@users.noreply.github.com>
2018-06-20 15:12:45 +0200

    Update lib.js

--- lib.js
+++ lib.js
@@ -1512,7 +1512,7 @@ initSteem():
 function initLib(initSteem, callback) {
   setupLogging();
   // #93, use alternate websocket temporarily
-  steem.api.setOptions({ url: 'steemd.privex.io'});
+  steem.api.setOptions({ url: 'https://rpc.curiesteem.com'});
   // #71, no longer need to set this
   var processes = [
     function() {
